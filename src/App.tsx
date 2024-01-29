7fc16a99d0a00b02b942ba974e2e211a00b375af
yebin <yebinp1102@naver.com>
2024-01-29 22:34:50 +0900

    Feat: setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,56 @@
+import { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent} from '@syncfusion/ej2-react-popups';
+import './App.css';
 
-function App() {
+const App = () => {
+  const activeMenu = true;
+
+  useEffect(() => {
+
+  },[])
 
   return (
     <div>
-      <h1>Hello world</h1>
+      <BrowserRouter>
+        <div className='flex relative dark:bg-main-dark-bg'>
+          <div className='fixed right-4 bottom-4' style={{ zIndex: '1000'}}>
+            <TooltipComponent content="Settings" position="TopCenter" >
+              <button 
+                type='button' 
+                className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
+                style={{background: 'blue', borderRadius: '50%'}}  
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+
+          {activeMenu ? (
+            <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+              Sidebar
+            </div>
+          ): (
+            <div className='w-0 dark:bg-secondary-dark-bg'>
+              Sidebar w-0
+            </div>
+          )}
+
+          <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+                Navbar
+            </div>
+          </div>
+
+
+          <div>
+            <Routes>
+            <Route path='/' element="Home" />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   )
 }
