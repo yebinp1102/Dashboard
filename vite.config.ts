cee939dd4ccc9ef830d19ce100c492812147a56c
yebin <yebinp1102@naver.com>
2024-05-20 22:34:48 +0900

    worklofws

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Dashboard/'
+  base: '/Dashboard/',
+  build: {
+    chunkSizeWarningLimit: 1600
+  }
 })
